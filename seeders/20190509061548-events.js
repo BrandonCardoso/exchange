5e013b60b37b2095582fb035ae0708e1b8efa61d
Brandon Cardoso <1226170+BrandonCardoso@users.noreply.github.com>
2019-05-11 23:15:00 -0400

    update event seed data location to be easier to debug

--- seeders/20190509061548-events.js
+++ seeders/20190509061548-events.js
@@ -8,7 +8,7 @@ module.exports = {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam maximus ut arcu nec efficitur. Aliquam et vulputate mauris. Pellentesque in sagittis quam. Nam lobortis molestie felis, a aliquam tellus finibus quis. Nullam sagittis quam finibus est vehicula fermentum. Aliquam non pellentesque felis, non finibus orci. Ut vehicula elit pharetra, lobortis risus et, consequat purus.',
         start_time: '2019-05-09 04:42:05',
         end_time: '2019-05-09 06:42:05',
-        location: '{"address":"12 Ulu, Sumatera Selatan, Indonesia","latlng":{"lat":-2.98982,"lng":104.772},"room":987}',
+        location: '{"room":"Top Floor","address":"301 Front Street West, Toronto, Ontario, Canada","latlng":{"lat":43.644,"lng":-79.3886}}',
         organizer_id: 1,
         created_at: new Date(),
         updated_at: new Date()
@@ -18,7 +18,7 @@ module.exports = {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam maximus ut arcu nec efficitur. Aliquam et vulputate mauris. Pellentesque in sagittis quam. Nam lobortis molestie felis, a aliquam tellus finibus quis. Nullam sagittis quam finibus est vehicula fermentum. Aliquam non pellentesque felis, non finibus orci. Ut vehicula elit pharetra, lobortis risus et, consequat purus.',
         start_time: '2019-05-09 04:42:05',
         end_time: '2019-05-09 06:42:05',
-        location: '{"address":"12 Ulu, Sumatera Selatan, Indonesia","latlng":{"lat":-2.98982,"lng":104.772},"room":987}',
+        location: '{"room":"Top Floor","address":"301 Front Street West, Toronto, Ontario, Canada","latlng":{"lat":43.644,"lng":-79.3886}}',
         organizer_id: 2,
         created_at: new Date(),
         updated_at: new Date()
