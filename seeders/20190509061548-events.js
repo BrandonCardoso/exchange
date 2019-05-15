894295f5717708cbb7ee6ed4b1c5a7e1c19830c5
Brandon Cardoso <1226170+BrandonCardoso@users.noreply.github.com>
2019-05-14 20:33:59 -0400

    add groups to event seeder data

--- seeders/20190509061548-events.js
+++ seeders/20190509061548-events.js
@@ -4,6 +4,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Events', [
       {
+        event_id: 1,
         name: 'Sample Event #1',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam maximus ut arcu nec efficitur. Aliquam et vulputate mauris. Pellentesque in sagittis quam. Nam lobortis molestie felis, a aliquam tellus finibus quis. Nullam sagittis quam finibus est vehicula fermentum. Aliquam non pellentesque felis, non finibus orci. Ut vehicula elit pharetra, lobortis risus et, consequat purus.',
         start_time: '2019-05-09 04:42:05',
@@ -14,6 +15,7 @@ module.exports = {
         updated_at: new Date()
       },
       {
+        event_id: 2,
         name: 'Sample Event #2',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam maximus ut arcu nec efficitur. Aliquam et vulputate mauris. Pellentesque in sagittis quam. Nam lobortis molestie felis, a aliquam tellus finibus quis. Nullam sagittis quam finibus est vehicula fermentum. Aliquam non pellentesque felis, non finibus orci. Ut vehicula elit pharetra, lobortis risus et, consequat purus.',
         start_time: '2019-05-09 04:42:05',
@@ -24,9 +26,37 @@ module.exports = {
         updated_at: new Date()
       }
     ])
+    .then(() => {
+      return queryInterface.bulkInsert('Groups', [
+        {
+          event_id: 1,
+          name: 'Participants',
+          max_participants: 20,
+          created_at: new Date(),
+          updated_at: new Date()
+        },
+        {
+          event_id: 2,
+          name: 'Group 1',
+          max_participants: 10,
+          created_at: new Date(),
+          updated_at: new Date()
+        },
+        {
+          event_id: 2,
+          name: 'Group 2',
+          max_participants: 30,
+          created_at: new Date(),
+          updated_at: new Date()
+        }
+      ])
+    })
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('Events', null, {})
+      .then(() => {
+        return queryInterface.bulkDelete('Groups', null, {})
+      })
   }
 };
